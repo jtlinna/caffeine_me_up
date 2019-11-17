44e64115db53aca64f289910823b571d226fc529
jukkatl <jukka.linna@dazzle.rocks>
2019-11-18 01:40:42 +0200

    Don't forget this dot

--- lib/models/group_data.dart
+++ lib/models/group_data.dart
@@ -7,7 +7,7 @@ class GroupData {
   List<UserData> members;
 
   GroupData(
-      {groupId, this.groupName, this.isPrivate, this.members});
+      {this.groupId, this.groupName, this.isPrivate, this.members});
   @override
   String toString() =>
       'GroupData $groupId -- Group name $groupName -- Is private $isPrivate -- Members $members';
